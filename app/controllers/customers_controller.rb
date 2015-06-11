d553a6e2266905d893b75d146d4d59b8bf4c17ca
Kevin Guran <support@mantraviz.com>
2015-06-10 23:28:16 -0400

    add customer index page. fix work orders table

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,11 +1,15 @@
 class CustomersController < ApplicationController
+  def index
+    @customers = Customer.all
+  end
+
   def new
     @customer = Customer.new
   end
 
   def create
     @customer = Customer.create(customer_params)
-    redirect_to technicians_path
+    redirect_to customers_path
   end
 
   private
